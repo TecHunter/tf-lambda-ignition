5bbbc487d51e2158201f4f6f46f81f25063e173e
Anthony Baschenis <abaschenis@serial.ch>
2020-10-08 13:49:47 +0200

    missing resource for root

--- lambda.tf
+++ lambda.tf
@@ -55,6 +55,10 @@ resource "aws_api_gateway_rest_api" "ApiGateway" {
 
 #### METHODS
 # GET /
+resource "aws_api_gateway_resource" "ignition" {
+  rest_api_id = aws_api_gateway_rest_api.ApiGateway.id
+  parent_id = aws_api_gateway_rest_api.ApiGateway.root_resource_id
+}
 resource "aws_api_gateway_method" "root" {
    rest_api_id   = aws_api_gateway_rest_api.ApiGateway.id
    resource_id   = aws_api_gateway_rest_api.ApiGateway.root_resource_id
