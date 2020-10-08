69a3b5443c7f9fda4d991f2afe29518d2944381a
Anthony Baschenis <abaschenis@serial.ch>
2020-10-08 16:43:30 +0200

    typo in response id

--- lambda.tf
+++ lambda.tf
@@ -107,7 +107,7 @@ resource "aws_api_gateway_method_response" "root_response_200" {
   status_code = "200"
 }
 
-resource "aws_api_gateway_integration_response" "ignition" {
+resource "aws_api_gateway_integration_response" "root" {
   rest_api_id = aws_api_gateway_rest_api.ApiGateway.id
   resource_id = aws_api_gateway_rest_api.ApiGateway.root_resource_id
   http_method = aws_api_gateway_method.get-root.http_method
