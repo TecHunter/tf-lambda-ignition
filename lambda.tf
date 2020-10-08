1ba2987c3900b07d787bee540d433fc3011d384c
Anthony Baschenis <abaschenis@serial.ch>
2020-10-08 13:51:05 +0200

    fix typo

--- lambda.tf
+++ lambda.tf
@@ -55,13 +55,13 @@ resource "aws_api_gateway_rest_api" "ApiGateway" {
 
 #### METHODS
 # GET /
-resource "aws_api_gateway_resource" "ignition" {
+resource "aws_api_gateway_resource" "root" {
   rest_api_id = aws_api_gateway_rest_api.ApiGateway.id
   parent_id = aws_api_gateway_rest_api.ApiGateway.root_resource_id
 }
 resource "aws_api_gateway_method" "root" {
    rest_api_id   = aws_api_gateway_rest_api.ApiGateway.id
-   resource_id   = aws_api_gateway_rest_api.ApiGateway.root_resource_id
+   resource_id   = aws_api_gateway_resource.root.id
    http_method   = "GET"
    authorization = "NONE"
 }
