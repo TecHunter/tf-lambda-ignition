a7345f9b5bac6c1d545761c30aba55e54fbc56df
Anthony Baschenis <abaschenis@serial.ch>
2020-10-08 13:52:00 +0200

    missing path part

--- lambda.tf
+++ lambda.tf
@@ -58,6 +58,7 @@ resource "aws_api_gateway_rest_api" "ApiGateway" {
 resource "aws_api_gateway_resource" "root" {
   rest_api_id = aws_api_gateway_rest_api.ApiGateway.id
   parent_id = aws_api_gateway_rest_api.ApiGateway.root_resource_id
+  path_part = ""
 }
 resource "aws_api_gateway_method" "root" {
    rest_api_id   = aws_api_gateway_rest_api.ApiGateway.id
