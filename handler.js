f06ba71043ddd12afe23c6123e1ba57948715b87
Anthony Baschenis <abaschenis@serial.ch>
2020-10-09 13:33:48 +0200

    use ed25519, add no-cache header

--- handler.js
+++ handler.js
@@ -6,6 +6,9 @@ exports.handler = (event, context, callback) => {
     callback(null,
         {
             "statusCode": 200,
+            "headers": {
+                "Cache-Control": "no-cache, max-age=0"
+            },
             "body": JSON.stringify(content),
             "isBase64Encoded": false
         });
